44e50b2cd0161342522eedf7a791b76979de0342
Esataydin <51443456+Esataydin@users.noreply.github.com>
2023-12-21 17:24:02 +0300

    Like option are added, now is functional.

---
+++ database.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from flask_login import UserMixin
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/default.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Bootstrap(app)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    surname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    posts = relationship("BlogPost", back_populates="author")
+    comments = relationship("Comment", back_populates="comment_author")
+
+class BlogPost(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    author = relationship("User", back_populates="posts")
+
+    title = db.Column(db.String(250), nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    category = db.Column(db.Text)
+    like_num = db.Column(db.Integer)
+    img_url = db.Column(db.String(250), nullable=False)
+    likes = db.Column(db.String(250))
+
+    # ***************Parent Relationship*************#
+    comments = relationship("Comment", back_populates="parent_post")
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment_author = relationship("User", back_populates="comments")
+    like_num = db.Column(db.Integer)
+
+    # ***************Child Relationship*************#
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    parent_post = relationship("BlogPost", back_populates="comments")
+    text = db.Column(db.Text, nullable=False)
+
+
+
+
+
+with app.app_context():
+    db.create_all()
--- server.py
+++ server.py
@@ -1,23 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, request, abort, flash
-from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, redirect, url_for, request, abort, flash
 from flask_ckeditor import CKEditor
-from werkzeug.security import generate_password_hash, check_password_hash
-import datetime
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from functools import wraps
-from forms import CreatePostForm, LoginForm, RegisterForm, CommentForm
-from sqlalchemy.orm import relationship
+from flask_login import login_user, LoginManager, login_required, current_user, logout_user
 from flask_gravatar import Gravatar
+from werkzeug.security import generate_password_hash, check_password_hash
 
+from forms import CreatePostForm, LoginForm, RegisterForm, CommentForm
+from database import app, db, User, BlogPost, Comment
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+import datetime
+from functools import wraps
+
 ckeditor = CKEditor(app)
-Bootstrap(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/default.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
@@ -31,52 +24,9 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(UserMixin, db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    surname = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.String(15), nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    posts = relationship("BlogPost", back_populates="author")
-    comments = relationship("Comment", back_populates="comment_author")
-
-
-class BlogPost(db.Model):
-    __tablename__ = "posts"
-    id = db.Column(db.Integer, primary_key=True)
-
-    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    author = relationship("User", back_populates="posts")
-
-    title = db.Column(db.String(250), nullable=False)
-    subtitle = db.Column(db.String(250), nullable=False)
-    date = db.Column(db.String(250), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    category = db.Column(db.Text)
-    like_num = db.Column(db.Integer)
-    img_url = db.Column(db.String(250), nullable=False)
-
-    # ***************Parent Relationship*************#
-    comments = relationship("Comment", back_populates="parent_post")
-
-
-class Comment(db.Model):
-    __tablename__ = "comments"
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    comment_author = relationship("User", back_populates="comments")
-    like_num = db.Column(db.Integer)
-
-    # ***************Child Relationship*************#
-    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
-    parent_post = relationship("BlogPost", back_populates="comments")
-    text = db.Column(db.Text, nullable=False)
-
-with app.app_context():
-    db.create_all()
-
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/", methods=["GET"])
 def get_all_posts():
@@ -91,26 +41,6 @@ def get_category(name):
         return render_template(f"{name.lower()}.html", all_posts=posts)
     else:
         return redirect(url_for('get_all_posts'))
-
-
-@app.route("/post/<int:index>", methods=['GET', 'POST'])
-def show_post(index):
-    form = CommentForm()
-    requested_post = BlogPost.query.get(index)
-    if form.validate_on_submit():
-        if not current_user.is_authenticated:
-            flash("You need to login or register to comment.")
-            return redirect(url_for("login"))
-
-        new_comment = Comment(
-            text=form.comment_text.data,
-            comment_author=current_user,
-            parent_post=requested_post,
-            like_num=0
-        )
-        db.session.add(new_comment)
-        db.session.commit()
-    return render_template("post.html", post=requested_post, form=form, current_user=current_user)
 
 
 #Create owner/owner/admin-only decorator
@@ -139,18 +69,52 @@ def owner_only(f):
     return decorated_function
 
 
-@app.route("/post/<int:post_id>/delete-comment/<int:comment_id>", methods=['GET', 'DELETE', 'POST'])
-@owner_only
-def delete_comment(post_id, comment_id):
-    print(comment_id)
-    comment = db.session.query(Comment).get(comment_id)
-    if comment:
-        db.session.delete(comment)
+@app.route("/new-post", methods=["POST", "GET"])
+@login_required
+def new_post():
+    if current_user:
+        form = CreatePostForm()
+        if form.validate_on_submit():
+            new_blog_post = BlogPost(
+                title=form.title.data,
+                subtitle=form.subtitle.data,
+                category=form.category.data,
+                author=current_user,
+                like_num=0,
+                likes="1",
+                img_url=form.img_url.data,
+                body=form.body.data,
+                date=datetime.datetime.now().strftime("%d/%m/%y")
+            )
+            db.session.add(new_blog_post)
+            db.session.commit()
+            return redirect(url_for('get_all_posts'))
+        return render_template("make-post.html", form=form)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route("/post/<int:index>", methods=['GET', 'POST'])
+def show_post(index):
+    form = CommentForm()
+    requested_post = BlogPost.query.get(index)
+    if form.validate_on_submit():
+        if not current_user.is_authenticated:
+            flash("You need to login or register to comment.")
+            return redirect(url_for("login"))
+
+        new_comment = Comment(
+            text=form.comment_text.data,
+            comment_author=current_user,
+            parent_post=requested_post,
+            like_num=0
+        )
+        db.session.add(new_comment)
         db.session.commit()
-    return redirect(url_for("show_post", index=post_id))
+    return render_template("post.html", post=requested_post, form=form, current_user=current_user)
 
 
-@app.route("/edit-post/<post_id>", methods=["GET", "POST", "PUT"])
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST", "PUT"])
 @owner_only
 def edit_post(post_id):
     post = db.session.query(BlogPost).get(post_id)
@@ -169,36 +133,12 @@ def edit_post(post_id):
         post.img_url = edit_form.img_url.data
         post.body = edit_form.body.data
         db.session.commit()
-        return redirect(url_for('get_all_posts'))
+        return redirect(url_for('show_post', index=post_id))
 
     return render_template("make-post.html", form=edit_form, is_edit=post_id)
 
 
-@app.route("/new-post", methods=["POST", "GET"])
-@login_required
-def new_post():
-    if current_user:
-        form = CreatePostForm()
-        if form.validate_on_submit():
-            new_blog_post = BlogPost(
-                title=form.title.data,
-                subtitle=form.subtitle.data,
-                category=form.category.data,
-                author=current_user,
-                like_num=0,
-                img_url=form.img_url.data,
-                body=form.body.data,
-                date=datetime.datetime.now().strftime("%d/%m/%y")
-            )
-            db.session.add(new_blog_post)
-            db.session.commit()
-            return redirect(url_for('get_all_posts'))
-        return render_template("make-post.html", form=form)
-    else:
-        return redirect(url_for('login'))
-
-
-@app.route("/delete/<post_id>", methods=["GET", "DELETE"])
+@app.route("/delete/<int:post_id>", methods=["GET", "DELETE"])
 @owner_only
 def delete_post(post_id):
     post = db.session.query(BlogPost).get(post_id)
@@ -207,19 +147,38 @@ def delete_post(post_id):
         db.session.commit()
     return redirect(url_for('get_all_posts'))
 
+@app.route("/like/post/<int:post_id>/<page>", methods=["GET", "POST", "PUT","DELETE"])
+@login_required
+def like_post(post_id, page):
+    post = db.session.query(BlogPost).get(post_id)
+    if post.likes == None:
+        post.likes = f"{current_user.id}"
+    else:
+        liked_users = post.likes.split(",")
+        if str(current_user.id) in liked_users:
+            post.likes = post.likes.replace(f",{current_user.id}", "")
+            post.like_num -= 1
+        else:
+            post.likes = post.likes + f",{current_user.id}"
+            post.like_num += 1
+    post.like_num = len(post.likes.split(",")) - 1
+    db.session.commit()
+    print(post.likes)
+    if page == "home":
+        return redirect(url_for("get_all_posts"))
+    else:
+        return redirect(url_for("show_post", index=post_id))
 
-@app.route("/categories")
-def categories():
-    return render_template("categories.html")
 
-@app.route("/profil")
-def profil():
-    return render_template("profil.html")
+@app.route("/post/<int:post_id>/delete-comment/<int:comment_id>", methods=['GET', 'DELETE', 'POST'])
+@owner_only
+def delete_comment(post_id, comment_id):
+    comment = db.session.query(Comment).get(comment_id)
+    if comment:
+        db.session.delete(comment)
+        db.session.commit()
+    return redirect(url_for("show_post", index=post_id))
 
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -290,12 +249,6 @@ def show_profile(user_id):
     except:
         return redirect(url_for("get_all_posts"))
 
-@app.route("/get_all_users")
-def get_all_users():
-    users = User.query.all()
-    for user in users:
-        print(f"User {user.id}: {user.name} {user.surname}, Email: {user.email}, Phone: {user.phone}")
-    return "Users printed in the terminal."
 
 if __name__ == "__main__":
     app.run(debug=True)
